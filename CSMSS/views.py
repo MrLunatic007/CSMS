235e2794a99a0b82d3718742a4c8441a8ee1d7a7
MrLunatic007 <mrlunatic.davis@gmai.com>
2024-10-05 22:00:02 +0300

    modified:   CSMS/__pycache__/settings.cpython-311.pyc 	modified:   CSMS/settings.py 	new file:   CSMSS/__pycache__/forms.cpython-311.pyc 	modified:   CSMSS/__pycache__/urls.cpython-311.pyc 	modified:   CSMSS/__pycache__/views.cpython-311.pyc 	modified:   CSMSS/forms.py 	new file:   CSMSS/migrations/__pycache__/__init__.cpython-311.pyc 	renamed:    CSMSS/templates/index.html -> CSMSS/templates/home.html 	modified:   CSMSS/urls.py 	modified:   CSMSS/views.py 	modified:   students/__pycache__/urls.cpython-311.pyc 	modified:   teachers/__pycache__/urls.cpython-311.pyc

--- CSMSS/views.py
+++ CSMSS/views.py
@@ -1,5 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login as auth_login
+from django.contrib import messages
+from .forms import LoginForm  # Assuming you have a LoginForm
 
 # Create your views here.
-def index(request):
-    return render(request, 'index.html')
+def home(request):
+    return render(request, 'home.html')
+
+def login(request):
+    return render(request, 'login/login.html')
